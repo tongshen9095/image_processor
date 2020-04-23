cb4a2b4df458a34906f216cfb1fbdf9c85814885
tong shen <ts325@duke.edu>
2020-04-23 01:57:27 -0400

    test verifyInfo

---
+++ test_server.py
@@ -0,0 +1,14 @@
+import pytest
+
+sample_dict = {"name": str, "b64str": str}
+in_dict1 = {"imgname": "apple.jpg", "b64str": 123}
+expt1 =  (False, "name key not found")
+in_dict2 = {"name": "apple.jpg", "b64str": 123}
+expt2 = (False, "b64str value not correct type")
+in_dict2 = {"name": "apple.jpg", "b64str": "123"}
+expt3 = (True, "")
+
+def test_verifyInfo(in_dict, sample_dict, expt):
+    from server import verifyInfo
+    ans = verifyInfo(in_dict, sample_dict)
+    assert ans == expt
