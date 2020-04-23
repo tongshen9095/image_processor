df760729365d601db32a42ae967162762564557f
tong shen <ts325@duke.edu>
2020-04-23 00:47:12 -0400

    test img2b64

--- test_client.py
+++ test_client.py
@@ -15,3 +15,15 @@ def test_parseName(fpath, expt):
     from client import parseName
     fname = parseName(fpath)
     assert fname == expt
+
+
+@pytest.mark.parametrize("fpath, expt", [
+    (fpath1, "/9j/4AAQSkZJRgABAgAA"),
+    (fpath2, "/9j/4AAQSkZJRgABAgAA"),
+    (fpath3, "/9j/4AAQSkZJRgABAgAA")
+])
+def test_img2b64(fpath, expt):
+    from client import img2b64
+    b64_str = img2b64(fpath)
+    assert b64_str[0:20] == expt
+
