9147da49efe46cce10ec2ae08199e3233fa993e7
tong shen <ts325@duke.edu>
2020-04-22 22:44:16 -0400

    add selectImg to uploadBtn cmd to test whether we need to keep the root window from appearing in the selectImg

--- client.py
+++ client.py
@@ -7,16 +7,19 @@ def designWindow():
     root = Tk()
 
     # Add upload button
-    upload_btn = ttk.Button(root, text="Upload")
+    upload_btn = ttk.Button(root, text="Upload", command=uploadBtnCmd)
     upload_btn.grid(column=0, row=0)
 
     root.mainloop()
     return
 
 
+def uploadBtnCmd():
+    fpath = selectImg()
+
 def selectImg():
     fpath = filedialog.askopenfilename()
     return fpath
 
 if __name__ == "__main__":
-    print(selectImg())
+    designWindow()
