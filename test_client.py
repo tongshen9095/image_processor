c2357954ec2c47de0d0e94e7663a7a5e5a8b23dd
tong shen <ts325@duke.edu>
2020-04-23 09:54:04 -0400

    modify test makeDict

--- test_client.py
+++ test_client.py
@@ -14,9 +14,13 @@ b64_str1 = "/9j/4AAQSkZJRgABAgAA"
 b64_str2 = "/9j/4AAQSkZJRgABAgAA"
 b64_str3 = "/9j/4AAQSkZJRgABAgAA"
 
-expt_dict1 = {"name": fname1, "dir": fpath1, "b64str": b64_str1}
-expt_dict2 = {"name": fname2, "dir": fpath2, "b64str": b64_str2}
-expt_dict3 = {"name": fname3, "dir": fpath3, "b64str": b64_str3}
+img_size1 = "512 x 512"
+img_size2 = "1024 x 1245"
+img_size3 = "228 x 369"
+
+expt_dict1 = {"name": fname1, "b64str": b64_str1, "imgsize": img_size1}
+expt_dict2 = {"name": fname2, "b64str": b64_str2, "imgsize": img_size2}
+expt_dict3 = {"name": fname3, "b64str": b64_str3, "imgsize": img_size3}
 
 @pytest.mark.parametrize("fpath, expt", [
     (fpath1, "acl1.jpg"),
@@ -52,12 +56,12 @@ def test_getImgSize(fpath, expt):
 
 
 @pytest.mark.parametrize("fname, fpath, b64_str, expt", [
-    (fname1, fpath1, b64_str1, expt_dict1),
-    (fname2, fpath2, b64_str2, expt_dict2),
-    (fname3, fpath3, b64_str3, expt_dict3),
+    (fname1, b64_str1, img_size1, expt_dict1),
+    (fname2, b64_str2, img_size2, expt_dict2),
+    (fname3, b64_str3, img_size3, expt_dict3),
 ])
-def test_makeDict(fname, fpath, b64_str, expt):
+def test_makeDict(fname, b64_str, img_size, expt):
     from client import makeDict
-    in_dict = makeDict(fname, fpath, b64_str)
+    in_dict = makeDict(fname, b64_str, img_size)
+    expt["timestamp"] = in_dict["timestamp"]
     assert in_dict == expt
-
