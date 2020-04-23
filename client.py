f3909fdd129808c446258f3926a781089a698620
tong shen <ts325@duke.edu>
2020-04-23 00:36:41 -0400

    add img2b64

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import messagebox
 from tkinter import filedialog
 import os
+import base64
 
 def designWindow():
     root = Tk()
@@ -39,6 +40,13 @@ def parseName(fpath):
         str: File name.
     """
     return os.path.basename(fpath)
+
+
+def img2b64(fpath):
+    with open(fpath, "rb") as image_file:
+        b64_bytes = base64.b64encode(image_file.read())
+    b64_string = str(b64_bytes, encoding='utf-8')
+    return b64_string
 
 
 if __name__ == "__main__":
