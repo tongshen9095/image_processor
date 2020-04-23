9719262b24a2008eceb28c0b409ca32943c9d25f
tong shen <ts325@duke.edu>
2020-04-23 00:12:14 -0400

    fix typoL acl.1.jpg->acl1.jpg

--- test_client.py
+++ test_client.py
@@ -7,7 +7,7 @@ fpath3 = common_dir +  "synpic50411.jpg"
 
 
 @pytest.mark.parametrize("fpath, expt", [
-    (fpath1, "acl.1.jpg"),
+    (fpath1, "acl1.jpg"),
     (fpath2, "esophagus2.jpg"),
     (fpath3, "synpic50411.jpg")
 ])
