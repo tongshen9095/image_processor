fd60fae87f549747ead0189962592475c7fe3cea
tong shen <ts325@duke.edu>
2020-04-22 22:32:17 -0400

    fix typo: botton -> button

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ def designWindow():
     root = Tk()
 
     # Add upload button
-    upload_btn = ttk.Botton(root, text="Upload")
+    upload_btn = ttk.Button(root, text="Upload")
     upload_btn.grid(column=0, row=0)
 
     root.mainloop()
