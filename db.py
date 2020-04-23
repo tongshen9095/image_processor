b733bc74e55eed59060fcddf7615d2049d64c184
tong shen <ts325@duke.edu>
2020-04-23 11:03:24 -0400

    add initDb

--- db.py
+++ db.py
@@ -5,3 +5,10 @@ class Image(MongoModel):
     b64str = fields.CharField()
     timestamp = fields.CharField()
     imgsize = fields.CharField()
+
+def init_db():
+    """Connect to database."""
+    print("Connecting to database...")
+    connect("mongodb+srv://db_access:9095@bme547-tla9o.mongodb.net/"
+            "img?retryWrites=true&w=majority")
+    print("Connected to database")
