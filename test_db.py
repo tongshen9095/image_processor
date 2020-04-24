e3f9b61fca27bb80ae9c8968d5a48cc0eebc227b
tong shen <ts325@duke.edu>
2020-04-24 11:03:45 -0400

    test hasImg

---
+++ test_db.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.parametrize("img_name, expt", [
+    ("acl1.jpg", True),
+    ("acl2.jpg", True),
+    ("abc.jpg", False)
+
+])
+def test_hasImg(img_name, expt):
+    from db import hasImg
+    ans = hasImg(img_name)
+    assert ans == expt
