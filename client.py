e502ec237e1a313ed686339f70eb989996215c3d
tong shen <ts325@duke.edu>
2020-05-02 17:30:43 -0400

    use postcommand instead

--- client.py
+++ client.py
@@ -39,10 +39,10 @@ def mainWindow():
             if status:
                 img_choice_box["values"] = img_names 
         img_choice = StringVar()
-        img_choice_box = ttk.Combobox(window, textvariable=img_choice)
+        img_choice_box = ttk.Combobox(window, textvariable=img_choice,
+                                      postcommand=updateChoice)
         xp, yp = 55, 2
         img_choice_box.place(x=dw*xp//100, y=dh*yp//100)
-        img_choice_box.bind("<<ComboboxSelected>>", updateChoice)
 
         # Put a blank image
         img_obj = Image.open("./images/blank.png").resize((dw, dw))
